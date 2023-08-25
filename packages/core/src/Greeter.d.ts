e26a3c87555dac94b6a04c69864322f5f1f7d6dc
Tim <hello@timsmart.co>
2023-08-25 12:49:57 +1200

    remove build artifacts

--- packages/core/src/Greeter.d.ts
+++
@@ -1,6 +0,0 @@
-import { Context, Effect, Layer } from "effect";
-export interface Greeter {
-    readonly greet: Effect.Effect<never, never, string>;
-}
-export declare const Greeter: Context.Tag<Greeter, Greeter>;
-export declare const GreeterLive: Layer.Layer<never, never, Greeter>;
